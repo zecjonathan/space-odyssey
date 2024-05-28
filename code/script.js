7cbadf8b86b3f713ac25a4cf5221d75c1adb7e16
Jonathan <130445045+zecjonathan@users.noreply.github.com>
2024-05-28 09:07:51 +0200

    schenke

--- code/script.js
+++ code/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() { // fetching the header.html into any index.html file with header-container id
-    fetch('header.html')
+    fetch('addons/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-container').innerHTML = data;
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() { // fetching the heade
 });
 
 document.addEventListener("DOMContentLoaded", function() { // fetching the footer.html into any index.html file with footer-container id
-    fetch('footer.html')
+    fetch('addons/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-container').innerHTML = data;
