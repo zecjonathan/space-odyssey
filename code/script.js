bceccfcd770dfd720b3702d97f5712ddd476914d
zecjonathan <jonathanshnyder@icloud.com>
2024-08-10 16:34:05 +0200

    github dude

--- code/script.js
+++ code/script.js
@@ -1,32 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() { // ALL CODE IN HERE RUNNS AS SOON AS THE PAGE LOADS
-    fetch('addons/header.html') // fetching the header.html into any index.html file with header-container id
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header-container').innerHTML = data;
-        })
+fetch('addons/header.html') // fetching the header.html into any index.html file with header-container id
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-container').innerHTML = data;
+    })
 
-    fetch('addons/footer.html') // fetching the header.html into any index.html file with header-container id
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer-container').innerHTML = data;
-        })
+fetch('addons/footer.html') // fetching the header.html into any index.html file with header-container id
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-container').innerHTML = data;
+    })
 
-    // cookie x color moder fetcher start
-    let cookieColor = getCookie("color-mode"); // sets cookieColor var to getCookie function return for the cookie "color-mode"
-    const html = document.querySelector('html'); // selects the <html> element and assigns it to the constant variable html
-    if (cookieColor === "dark") { // if cookieColor (so from cookie browser data) is "dark", sets the 'color-mode' attribute of the <html> element to 'dark
-        html.setAttribute('color-mode', 'dark');
-    } 
-    if (cookieColor === "light") { // if cookieColor is "light", sets the 'color-mode' attribute of the <html> element to 'light'
-        html.setAttribute('color-mode', 'light');
-    } 
-    // cookie x color moder fetcher end
-});
+// cookie x color moder fetcher start
+let cookieColor = getCookie("color-mode"); // sets cookieColor var to getCookie function return for the cookie "color-mode"
+const html = document.querySelector('html'); // selects the <html> element and assigns it to the constant variable html
+if (cookieColor === "dark") { // if cookieColor (so from cookie browser data) is "dark", sets the 'color-mode' attribute of the <html> element to 'dark
+    html.setAttribute('color-mode', 'dark');
+} 
+if (cookieColor === "light") { // if cookieColor is "light", sets the 'color-mode' attribute of the <html> element to 'light'
+    html.setAttribute('color-mode', 'light');
+} 
+// cookie x color moder fetcher end
 
 function getCookie(cname) { // sets function to get cookie 
     let name = cname + "="; // sets var name to function input and "="
     let decodedCookie = decodeURIComponent(document.cookie); // gets all browser saved cookies
-    let ca = decodedCookie.split(';'); // splits all cookies by ";" so it has all cookies seperated
+    let ca = decodedCookie.split('%3B'); // splits all cookies by ";"(%3B) so it has all cookies seperated
     for(let i = 0; i <ca.length; i++) { // loops through each cookie
         let c = ca[i]; // gets the item (cookie)
         while (c.charAt(0) == ' ') { // makes cookie data to string so removes the stuff around like spaces and ' '
@@ -43,10 +41,10 @@ function toggleColorMode() { // function (gets called with button in header and 
     const html = document.querySelector('html'); // selects the <html> element and assigns it to the constant variable html
     if (html.getAttribute('color-mode') === 'light') { // if current color mode is light
         html.setAttribute('color-mode', 'dark'); // set attribute dark
-        document.cookie = "color-mode=dark"; // push to browser cookie
+        document.cookie = "color-mode=dark;max-age:31536000"; // push to browser cookie                 illegal weil zu lange gespeichert aber für schule juckt ig
     } else {
         html.setAttribute('color-mode', 'light'); // set attribute dark
-        document.cookie = "color-mode=light"; // push to browser cookie
+        document.cookie = "color-mode=light;max-age:31536000"; // push to browser cookie
     }
 }
 
